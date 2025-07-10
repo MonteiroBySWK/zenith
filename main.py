07e9938588f2b66bd105fed1c9d33fd88cc21c7f
MonteiroBySWK <eumonteiro.ofc@gmail.com>
2025-07-10 19:10:18 -0300

    feat: modularização da main.py

--- main.py
+++ main.py
@@ -8,23 +8,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<div style='height: 100vh; width: 100%; display: flex; justify-content: center; align-items: center;'><h1>It's Work!</h1></div>"
+    return render_template("index.html")
 
 @app.route("/upload", methods=["GET"])
 def upload():
     return render_template("upload.html")
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    if 'file' not in request.files: 
-        return jsonify({"error": "Nenhum arquivo enviado"}), 400
-    
 
-    file = request.files['file']
-
-    if file.filename.endswith('.csv'):
+def read_file(file):
+    if file.filename.endswith(".csv"):
         df = pd.read_csv(file)
-    elif file.filename.endswith(('.xls', '.xlsx')):
+    elif file.filename.endswith((".xls", ".xlsx")):
         df = pd.read_excel(file)
     else:
         return jsonify({"error": "Formato de arquivo não suportado"}), 400
@@ -32,11 +26,35 @@ def predict():
     result = {
         "linhas": df.shape[0],
         "colunas": df.shape[1],
-        "colunas_nome": df.columns.tolist()
+        "colunas_nome": df.columns.tolist(),
     }
+
+    return result
+
+def predict(data):
+    # Modelo de predição aqui, retornar um excel? 
+    # Não sei se é pra retornar a previsão de um dia ou de uma determinada serie
+    ...
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Essa função deve ser melhor pensada
+    if "file" not in request.files:
+        return jsonify({"error": "Nenhum arquivo enviado"}), 400
+
+    file = request.files["file"]
+
+    result = read_file(file)
+
+    # predict(data)
 
     return jsonify(result), 200
 
+
+@app.route("/report")
+def report():
+    # gerar o relatorio ou algo assim não se vai ficar assim no final
+    ...
 
 @app.route("/dashboard")
 def dashboard():
